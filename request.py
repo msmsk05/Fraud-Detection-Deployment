d1c7950e2036ba89f7421ec955be2dca5e2707d1
msmsk05 <45575514+msmsk05@users.noreply.github.com>
2021-02-21 11:58:19 +0300

    updates

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/results'
-r = requests.post(url,json={"V14":-0.31, "V10":0.09, "V4":1.38, "V12":-0.62, "V17":0.21, "V11":-0.55})
+r = requests.post(url,json={"V4":1.38,"V10":0.09,"V11":-0.55,"V12":-0.62,"V14":-0.31,"V17":0.21})
 
 print(r.json())
